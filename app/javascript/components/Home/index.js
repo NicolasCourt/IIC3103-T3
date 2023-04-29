544a4e804c2fe80ddf5b102086642f8673750b74
NicolasCourt <ncourt@uc.cl>
2023-04-29 04:41:50 -0400

    firstpage

---
+++ app/javascript/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = (props) => {
+
+  return (
+    <div>Hello react</div>
+  )
+}
+
+export default Home
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'render#index'
+
+  match '*path', to: 'render#index', via: :all
 end
